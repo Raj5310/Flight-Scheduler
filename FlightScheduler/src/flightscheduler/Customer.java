343285c391313463f030cf7b8e24958aac00302c
raj5310 <rajpokhrel30@gmail.com>
2017-11-09 22:42:29 -0500

    Added different classes

---
+++ FlightScheduler/src/flightscheduler/Customer.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flightscheduler;
+
+/**
+ *
+ * @author Raj
+ */
+public class Customer
+{
+    private String name;
+    
+    public Customer(String name)
+    {
+        this.name = name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    
+}
